be33d67136d6cf9cd6f84b8067f5d54039e20c33
rjsnhk <nahakrajesh003@gmail.com>
2025-10-26 13:30:36 +0530

    add auto relode server

--- lmp-backend/server.js
+++ lmp-backend/server.js
@@ -8,9 +8,35 @@ const sessionRoutes = require("./routes/sessionRoutes");
 const questionRoutes = require("./routes/questionRoutes");
 const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
 const { protect } = require("./middlewares/authMiddleware");
+const axios = require("axios");
 
 
 const app = express();
+
+
+const url ="https://lmp-backend.onrender.com";
+const interval = 30000; // 30 seconds
+
+let lastReloadTime = "Not yet reloaded";
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then(() => {
+      lastReloadTime = new Date().toLocaleString("en-IN", {
+        timeZone: "Asia/Kolkata",
+      });
+      console.log(`✅ Website reloaded at ${lastReloadTime}`);
+    })
+    .catch((error) => {
+      console.error(`❌ Error reloading website: ${error.message}`);
+    });
+}
+setInterval(reloadWebsite, interval);
+
+// optional: trigger once on startup
+reloadWebsite();
+
+
 const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: "*",
@@ -40,7 +66,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads"),{}));
 
 
 app.get("/", (req, res) => {
-  res.send("API is running...");  
+  res.send(`Last time website reloaded: ${lastReloadTime}`);  
 });
 
 app.listen(PORT, () => {
